7219037d15e5cdc3ce74e3e9744cd54bf4028eaa
Olajire Taofeek <30867433+Oltao@users.noreply.github.com>
2023-05-08 13:17:44 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+const hoursEl = document.getElementById("hours");
+const minutesEl = document.getElementById("minutes");
+const secondsEl = document.getElementById("seconds");
+const ampmEl = document.getElementById("ampm");
+
+function digitalClock(){
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+    let ampm = "AM";
+
+    if (h == 0){
+        h = 12;
+    };
+
+    if (h > 11) {
+        h = h - 12;
+        ampm = "PM";
+    }
+
+    if (h < 10){
+        h = "0" + h;
+    } else h;
+    if (m < 10){
+        m = "0" + m;
+    } else m;
+    if (s < 10){
+        s = "0" + s;
+    } else s;
+
+    setTimeout(digitalClock);
+
+hoursEl.innerText = h;
+minutesEl.innerText = m;
+secondsEl.innerText = s;
+ampmEl.innerText = ampm;
+}
+
+digitalClock();
